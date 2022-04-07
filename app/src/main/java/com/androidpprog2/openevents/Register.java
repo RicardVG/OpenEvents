7184e5d05de8f0570721bd818ed4707c2edc4442
RicardVG <ricardvinolas10@gmail.com>
2022-04-07 18:33:21 +0200

    Layout Register

---
+++ app/src/main/java/com/androidpprog2/openevents/Register.java
@@ -0,0 +1,13 @@
+package com.androidpprog2.openevents;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
