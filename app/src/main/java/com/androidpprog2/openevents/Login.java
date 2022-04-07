8d66dd789f5a71d05db2c23d89352f9629bfb9a4
RicardVG <ricardvinolas10@gmail.com>
2022-04-07 19:08:44 +0200

    Pantalla de Login i Register feta, falta contactar amb la API

--- app/src/main/java/com/androidpprog2/openevents/Login.java
+++ app/src/main/java/com/androidpprog2/openevents/Login.java
@@ -2,7 +2,10 @@ package com.androidpprog2.openevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        TextView sign_up = this.findViewById(R.id.SignUp);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Register.newIntent(Login.this);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/androidpprog2/openevents/Register.java
+++ app/src/main/java/com/androidpprog2/openevents/Register.java
@@ -1,13 +1,20 @@
 package com.androidpprog2.openevents;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
+    public static Intent newIntent(Context packageContext) {
+        Intent intent = new Intent(packageContext, Register.class);
+        return intent;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.register);
     }
 }
