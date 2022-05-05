2775d344003bfed2e4b9ea9e1a977093e0556c59
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2022-05-05 19:11:08 +0200

    Event post api

--- app/src/main/java/com/androidpprog2/openevents/persistance/OpenEventsAPI.java
+++ app/src/main/java/com/androidpprog2/openevents/persistance/OpenEventsAPI.java
@@ -3,9 +3,15 @@ package com.androidpprog2.openevents.persistance;
 import com.androidpprog2.openevents.business.User;
 import com.androidpprog2.openevents.business.LoginRequest;
 
+import java.util.Date;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface OpenEventsAPI {
 
@@ -19,6 +25,17 @@ public interface OpenEventsAPI {
     Call<User> registerUser(
             @Body User user
     );
+
+    @POST("events")
+    Call<Event> createEvent(@Header("authorization") String token,
+                            @Part("name") RequestBody name,
+                            @Part MultipartBody.Part image,
+                            @Part("location") RequestBody location,
+                            @Part("description") RequestBody description,
+                            @Part("eventStart_date") Date eventStart_date,
+                            @Part("eventEnd_date") Date eventEnd_date,
+                            @Part("n_participators") RequestBody n_participators,
+                            @Part("type") RequestBody type);
 
 
 }
