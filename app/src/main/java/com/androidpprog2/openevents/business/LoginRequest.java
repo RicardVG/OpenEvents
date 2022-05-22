defb399a447bd6e02ed5d5bb64bea0877b12bcbc
RicardVG <ricardvinolas10@gmail.com>
2022-05-23 00:26:29 +0200

    Arreglant Warnings

--- app/src/main/java/com/androidpprog2/openevents/business/LoginRequest.java
+++ app/src/main/java/com/androidpprog2/openevents/business/LoginRequest.java
@@ -5,15 +5,11 @@ import java.io.Serializable;
 public class LoginRequest implements Serializable {
     private String email;
     private String password;
-    private String accessToken;
 
 
     public LoginRequest(String email, String password) {
         this.email = email;
         this.password = password;
-    }
-    public String getAccessToken() {
-        return accessToken;
     }
 
 }
--- app/src/main/java/com/androidpprog2/openevents/persistance/OpenEventsAPI.java
+++ app/src/main/java/com/androidpprog2/openevents/persistance/OpenEventsAPI.java
@@ -1,16 +1,10 @@
 package com.androidpprog2.openevents.persistance;
 
-import android.content.SharedPreferences;
-
 import com.androidpprog2.openevents.business.Event;
-import com.androidpprog2.openevents.business.User;
 import com.androidpprog2.openevents.business.LoginRequest;
-
+import com.androidpprog2.openevents.business.User;
 import java.util.ArrayList;
 import java.util.Date;
-
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -18,20 +12,16 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
-import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface OpenEventsAPI {
-
     @POST("users/login/")
     Call<LoginRequest> loginUser(
             @Body LoginRequest userAToken
     );
-
 
     @POST("users/")
     Call<User> registerUser(
@@ -40,7 +30,6 @@ public interface OpenEventsAPI {
 
     @GET("events")
     Call<ArrayList<Event>> getEvents(@Header("Authorization") String accessToken);
-
 
     @FormUrlEncoded
     @POST("events")
@@ -53,7 +42,6 @@ public interface OpenEventsAPI {
                             @Field("eventEnd_date") Date eventEnd_date,
                             @Field("n_participators") int n_participators,
                             @Field("type") String type);
-
     @GET("events/{id}")
     Call<ArrayList<Event>> getEvent(@Header("Authorization") String accessToken, @Path("id") int id);
 
@@ -69,12 +57,9 @@ public interface OpenEventsAPI {
     @GET("users")
     Call<ArrayList<User>> getUsers(@Header("Authorization") String accessToken);
 
-
     @GET("users/{id}/statistics")
     Call<User> getStatistics(@Header("Authorization") String accessToken, @Path("id") int id);
 
     @PUT("users")
-    Call<User> updateUser(@Header("authorization") String accessToken,
-                                   @Body User user);
-
+    Call<User> updateUser(@Header("authorization") String accessToken, @Body User user);
 }
